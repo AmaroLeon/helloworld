6f98e5790c930e91651a6c1c67d0d32de681fe91
AmaroLeon <84506289+AmaroLeon@users.noreply.github.com>
2024-12-13 09:27:27 +0100

    quit add2

--- app/api.py
+++ app/api.py
@@ -9,8 +9,6 @@ CALCULATOR = Calculator()
 api_application = Flask(__name__)
 HEADERS = {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"}
 
-
-
 @api_application.route("/")
 def hello():
     return "Hello from The Calculator!\n"
@@ -24,14 +22,6 @@ def add(op_1, op_2):
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
-
-@api_application.route("/calc/add2/<op_1>/<op_2>", methods=["GET"])
-def add(op_1, op_2):
-    try:
-        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
-        return ("{}".format(CALCULATOR.add(num_1, num_2)), http.client.OK, HEADERS)
-    except TypeError as e:
-        return (str(e), http.client.BAD_REQUEST, HEADERS)
 
 @api_application.route("/calc/substract/<op_1>/<op_2>", methods=["GET"])
 def substract(op_1, op_2):
